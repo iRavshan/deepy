1e5d5a9246cffc8bcc9dde3055651bc4e0fe1fb6
Gul1w <sodiqovagulshana@gmail.com>
2026-03-03 15:06:56 +0500

    feat: Add challenge detail page with navigation, tabs, and URL routing.

--- apps/challenges/urls.py
+++ apps/challenges/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from .views import challenge_list, challenge_detail, run_code_view, challenge_random, judge_submission_view, toggle_challenge_save
 
 
-
 urlpatterns = [
     path('', challenge_list, name='challenge_list'),
     path('toggle-save/', toggle_challenge_save, name='toggle_challenge_save'),
+    path('random/', challenge_random, name='challenge_random'),
     path('<slug:slug>/', challenge_detail, name='challenge_detail'),
     path('<slug:slug>/run/', run_code_view, name='run_code'),
-    path('random/', challenge_random, name='challenge_random'),
     path('<slug:slug>/judge_submission/', judge_submission_view, name='judge_submission'),
 ]
